11bd7e3c0caee1d706963d238af059d4becbf3e6
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2024-05-27 15:49:01 +0500

    Initial

---
+++ app/src/main/java/com/example/exampleandroid/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.exampleandroid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.exampleandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnChangeText.setOnClickListener {
+            binding.tvTextSalem.text = "It's Android Development"
+        }
+    }
+}
